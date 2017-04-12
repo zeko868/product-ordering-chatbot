c21c5b12b24ce014b7e7f80bc49efb48983db3c4
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-12 20:06:49 +0200

    a14

--- bot.php
+++ bot.php
@@ -10,11 +10,12 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$messageText = isset($input['entry'][0]['messaging'][0]['message']['text']) ? $input['entry'][0]['messaging'][0]['message']['text']: '';
+$postback = isset($input['entry'][0]['messaging'][0]['postback']['payload']) ? $input['entry'][0]['messaging'][0]['postback']['payload']: '';
 $response = null;
 //set Message
-if($messageText == "hi"){
-     $answer = ["attachment"=>[
+if($messageText == "hi") {
+    $answer = ["attachment"=>[
       "type"=>"template",
       "payload"=>[
         "template_type"=>"generic",
@@ -40,11 +41,14 @@ if($messageText == "hi"){
         ]
       ]
     ]];
-     $response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => $answer 
-];
+} else if($postback != ''){
+	$answer = $postback;
 }
+//send message to facebook bot
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
@@ -52,4 +56,4 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 if(!empty($input)){
 $result = curl_exec($ch);
 }
-curl_close($ch);?>
+curl_close($ch);
