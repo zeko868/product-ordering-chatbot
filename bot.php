8ecc49e24c70d86717f9ff7f6ccf8e13a20167e7
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-13 23:33:21 +0200

    make it better

--- bot.php
+++ bot.php
@@ -47,17 +47,7 @@ if($messageText == "hi"){
     'recipient' => [ 'id' => $senderId ],
     'message' => $answer 
 ];}
-if($postback != ''){
-	$answer = $postback;
-	$response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => $answer];
-}else{
-	$answer = 'ne';
-	$response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => $answer];
-}
+
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
