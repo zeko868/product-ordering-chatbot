94cb218a810dafdb295c48f44d7cfe159da4580b
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-13 23:02:43 +0200

    make it better

--- bot.php
+++ bot.php
@@ -12,15 +12,37 @@ $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
-//set Message
-if($messageText == "hi") {
-    $answer = "Hello";
-}
-//send message to facebook bot
-$response = [
+if($messageText == "hi"){
+     $answer = ["attachment"=>[
+      "type"=>"template",
+      "payload"=>[
+        "template_type"=>"generic",
+        "elements"=>[
+          [
+            "title"=>"Welcome to Peter\'s Hats",
+            "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+            "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+            "subtitle"=>"We\'ve got the right hat for everyone.",
+            "buttons"=>[
+              [
+                "type"=>"web_url",
+                "url"=>"https://petersfancybrownhats.com",
+                "title"=>"View Website"
+              ],
+              [
+                "type"=>"postback",
+                "title"=>"Start Chatting",
+                "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+              ]              
+            ]
+          ]
+        ]
+      ]
+    ]];
+     $response = [
     'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
-];
+    'message' => $answer 
+];}
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
