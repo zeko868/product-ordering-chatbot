a512e015535a586999776395fe89202fe527394f
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-15 13:41:20 +0200

    string

--- citanje.php
+++ citanje.php
@@ -3,6 +3,6 @@ $a = array();
 $array = array('a'=>'1','b'=>'2');
 array_push($a,$array);
 array_push($a,array('a'=>'3','b'=>'4'));
-$a2 =json_encode($a);
-print_r(str_replace($a2,'{','['));
+$a =json_encode($a);
+print_r($a2);
 ?>
