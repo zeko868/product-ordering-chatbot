d9c348cde0f3d8082161b9d6140115cfd7d08fba
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-15 12:00:16 +0200

    string

--- citanje.php
+++ citanje.php
@@ -2,10 +2,5 @@
 $xml = simplexml_load_string("informacije.xml");
 $dom = new DOMDocument();
 $dom->loadXML($xml);
-
-foreach($dom->getElementsByTagName('response') as $currency)
-{
-    echo $currency->employees[0]->employee[0]->getAttribute('firstname'), "\n";
-    echo "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n";
-}
+print_r($dom->getElementsByTagName('response'));
 
