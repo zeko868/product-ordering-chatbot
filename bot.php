1d7d2539110666877040416ca67d54c731a3ed1c
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-15 15:01:55 +0200

    string

--- bot.php
+++ bot.php
@@ -29,7 +29,7 @@ if (!empty($input['entry'][0]['messaging'])) {
 if($command == "konzultacije"){
 	$button = array();
 for($i=0;$i<3;$i++){
-	array_push($button, array('type'=>'postback', 'title'=>'Profesor $i', 'payload' => '$i'));
+	array_push($button, array('type'=>'postback', 'title'=>'Profesor ' . $i, 'payload' => $i));
 }
      $answer = [
       'type'=>'template',
