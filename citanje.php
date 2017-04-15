fc469d0c7e45708d16165895d7167f1eb2c5e773
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-15 12:28:43 +0200

    string

--- citanje.php
+++ citanje.php
@@ -1,5 +1,10 @@
 <?php
 $xml=simplexml_load_file('informacije.xml') or die("Error: Cannot create object");
+$item = $xml->employee[0];
+foreach($item->consultation as $i){
+			//$i->day.$i->time_from.$i->time_to
+			echo $i->term;
+		}
 echo $xml->employee[0]->firstname;
 
 ?>
