6804f97cc99c7847f758712e8d820c57a62ae634
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-15 13:40:35 +0200

    string

--- citanje.php
+++ citanje.php
@@ -4,5 +4,5 @@ $array = array('a'=>'1','b'=>'2');
 array_push($a,$array);
 array_push($a,array('a'=>'3','b'=>'4'));
 $a2 =json_encode($a);
-echo str_replace($a2,'{','[');
+print_r(str_replace($a2,'{','['));
 ?>
