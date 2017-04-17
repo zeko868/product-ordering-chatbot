428db3342249dc6e49118c54dc9a0da369b5aabc
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-17 09:09:55 +0200

    make it better

--- bot.php
+++ bot.php
@@ -47,21 +47,6 @@ $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $response = null;
 $command = "";
 
-$answer = [
-  "setting_type":"greeting",
-  "greeting":[
-    "text":"Hi {{user_first_name}}, welcome to this bot."
-  ]
-];
-		$response = [
-			'recipient' => [ 'id' => $senderId ],
-			'message' => [ 'attachment' => $answer ]
-		];
-
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 if(!empty($input)){
 	$result = curl_exec($ch);
 }
@@ -260,7 +245,7 @@ if ($response === null) {
 }
 
 
-
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
