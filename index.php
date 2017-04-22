417d71575f6279345aef38e9c9fcc3b0832179db
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 17:45:33 +0200

    make it better

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ curl_setopt($ch, CURLOPT_URL, "http://foi-konzultacije.info/curl.php?senderid=$s
 curl_setopt($ch, CURLOPT_HEADER, 0);
 $output = curl_exec($ch);
 curl_close($ch);
-echo $output[0];
+echo intval($output[0]);
