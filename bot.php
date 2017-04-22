16096282dbfd9206a979b55cc38b051c9b62414e
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 17:46:35 +0200

    make it better

--- bot.php
+++ bot.php
@@ -115,7 +115,7 @@ if (stripos($command, 'konzultacije') === 0) {
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	$output = curl_exec($ch);
 	curl_close($ch);
-	if(intval($output[0])===1){
+	if($output==='1'){
 		
 		preg_match($termRegex, $command, $termArray);
 		if (empty($termArray)) {
