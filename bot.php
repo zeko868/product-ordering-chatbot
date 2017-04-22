c809e0f4867e0c822c10a10a18324e283db5efc7
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 16:13:51 +0200

    make it better

--- bot.php
+++ bot.php
@@ -65,6 +65,14 @@ if(!empty($_GET)){
 			'recipient' => [ 'id' => $senderId ],
 			'message' => [ 'text' => $answer ]
 		];
+	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	if(!empty($input)){
+		$result = curl_exec($ch);
+	}
+	curl_close($ch);
 }
 
 if (!empty($input['entry'][0]['messaging'])) { 
