a0c925805e9b2cfc55f4fe9039e64dc11849fd56
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 16:16:56 +0200

    make it better

--- bot.php
+++ bot.php
@@ -57,23 +57,15 @@ $input = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $response = null;
 $command = "";
-
-if(!empty($_GET)){
+if($_GET['senderId']!=''){
 	$senderId = $_GET['senderId'];
 	$answer = "Uspješno ste izvršili autentikaciju.";
-		$response = [
-			'recipient' => [ 'id' => $senderId ],
-			'message' => [ 'text' => $answer ]
-		];
-	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-	if(!empty($input)){
-		$result = curl_exec($ch);
-	}
-	curl_close($ch);
+	$response = [
+		'recipient' => [ 'id' => $senderId ],
+		'message' => [ 'text' => $answer ]
+	];
 }
+
 
 if (!empty($input['entry'][0]['messaging'])) { 
 
