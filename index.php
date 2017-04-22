316f7897ba38d73f1473b901bbfbc5e64ac3030e
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 16:56:49 +0200

    make it better

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $dbHandler = new mysqli('foi-konzultacije.info', 'admin', 'f1f2f3f4', 'konzultacije');
 $dbHandler->set_charset("utf8");
 $command = "SELECT count(*) FROM users WHERE id = ".$_GET['senderid'].";";
-
+print_r($dbHandler);
 $resultSet = $dbHandler->query($command);
-print_r($resultSet);
+
 if(intval($resultSet->fetch_assoc()['count(*)']) === 0){
 	
 }
