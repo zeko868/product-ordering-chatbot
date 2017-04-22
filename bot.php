86882348aab1143afb313bef539d6736511b01fb
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-22 16:04:19 +0200

    make it better

--- bot.php
+++ bot.php
@@ -109,7 +109,7 @@ $substitutes = [
 $dayNames = ['ponedjeljak', 'utorak', 'srijeda', 'četvrtak', 'petak', 'subota', 'nedjelja'];
 $termRegex = '/(-|(' .implode('|', $dayNames) . ') \d{2}:\d{2} - \d{2}:\d{2})$/u';
 if(stripos($command, 'autentikacija') === 0){
-	$answer = "Poterebna je autentikacija";
+	$answer = "Poterebna je autentikacija za rad u sustavu. Za autentikaciju pristupite linku: http://foi-konzultacije.info/prijava.php?senderid=".$senderId;
 		$response = [
 			'recipient' => [ 'id' => $senderId ],
 			'message' => [ 'text' => $answer ]
