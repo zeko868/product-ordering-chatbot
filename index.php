2d34077a1a01b2e7a1931e1be10347426ae79cc2
marmihajl <marin.mihajlovic1994@gmail.com>
2017-04-23 09:09:28 +0200

    make it better

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <?php
-$senderId = $_GET['senderid'];
+$vars="{ 'channels': 
+	[ 'SMS' ], 
+	'destinations': 
+		[ 
+			{ 'phoneNumber': '0955541310' } 
+		], 
+	'sms': { 
+		'sender': 'smssender', 
+		'text': 'Sms text' 
+	} 
+}";
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://foi-konzultacije.info/curl.php?senderid=$senderId");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$output = curl_exec($ch);
-curl_close($ch);
-echo $output[0];
+curl_setopt($ch, CURLOPT_URL,"https://omni1.mobile-gw.com:9010/v1/omni/message");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+	'Host: omni1.mobile-gw.com:9010',
+	'Authorization: Basic aGVsaXhuZWJ1bGE6K0t7YEc9LWNgNEJ0bjtVWA==',
+	'Content-Type: application/json',
+	'Accept: application/json'
+];
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+//$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+echo $ch;
+//print  $server_output ;
