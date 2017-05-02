bdec0c213ba420d4180e940c008dd7d980d6f04e
marmihajl <marin.mihajlovic1994@gmail.com>
2017-05-03 01:39:12 +0200

    make it better

--- bot.php
+++ bot.php
@@ -167,6 +167,7 @@ else if (stripos($command, 'konzultacije') === 0) {
 			curl_setopt($ch, CURLOPT_HEADER, 0);
 			$output = curl_exec($ch);
 			curl_close($ch);
+			exit();
 
 		} else {
 			if ($term === null) {
