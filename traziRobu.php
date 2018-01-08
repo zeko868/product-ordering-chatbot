3b6051e660d9279aec519e40163deff84cbfb313
marselek <titanlook20@gmail.com>
2018-01-09 00:15:26 +0100

    refactor: poboljšanje u brzini izvođenja

--- traziRobu.php
+++ traziRobu.php
@@ -2,22 +2,23 @@
 ob_start();
 header('Content-Type: text/html; charset=utf-8');
 ini_set("allow_url_fopen", 1);
-$trazilica = "https://www.links.hr/hr/search?q=";
-$pojamZaPretragu = "grafička"; //Trenutno hardcoded
-$url = $trazilica . urlencode($pojamZaPretragu);
+$stranica = "https://www.links.hr";
+$trazilica= "/hr/search?q=";
+$pojamZaPretragu = "grafička"; //Trenutno hardcoded 
+$url = $stranica. $trazilica . urlencode($pojamZaPretragu);
 
 $lines = file($url);
 
 $nadjeno = TRUE;
-echo $url . "<br/>";
+
 
 for ($index = 0; $index < count($lines); $index++) {
     $polje = array();
-    /*if (strpos($lines[$index], "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima")) {
+    if (strpos($lines[$index], "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima")) {
         echo "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima <br/>";
         $nadjeno = FALSE;
         break;
-    }*/
+    }
     if (strpos($lines[$index], "div class=\"product-item\"")) {
         for ($i = $index; $i < $index + 54; $i++) {
             $polje[] = $lines[$i];
@@ -29,23 +30,29 @@ for ($index = 0; $index < count($lines); $index++) {
 }
 
 if ($nadjeno) {
-    echo json_encode($obj, JSON_UNESCAPED_UNICODE);
+    echo json_encode($obj, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
 }
 
 function parsiranjeProizvoda($polje) {
     $naziv = "";
     $cijena = "";
+    $link= "";
     for ($i = 0; $i < count($polje); $i++) {
         if (strpos($polje[$i], "product-title")) {
             $naziv = parsirajIme($polje[$i + 1]);
+            continue;
             //echo "Naziv: " . $naziv . "<br>";
         }
         if (strpos($polje[$i], "price actual-price")) {
             $cijena = parsirajCijenu($polje[$i]);
+            continue;
             //echo "Cijena: " . $cijena . "<br>";
         }
+        if(strpos($polje[$i], "a href")){
+            $link= parsirajLink($polje[$i]);
+        }
     }
-    $obj = (object) ['naziv' => $naziv, 'cijena' => $cijena];
+    $obj = (object) ['naziv' => $naziv, 'link' => $link, 'cijena' => $cijena];
     return $obj;
 }
 
@@ -58,6 +65,11 @@ function parsirajCijenu($linija) {
     preg_match_all("/(?<=\>).+?(?=\<)/", $linija, $match);
     $cijena = $match[0][0] . "," . $match[0][1];
     return $cijena;
+}
+
+function parsirajLink($linija){
+    preg_match("/\"(.*?)\"/", $linija, $match);
+    return $match[1];
 }
 ?>
 <!DOCTYPE html>
