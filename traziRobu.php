119cfcfd6805a2db3751e1289703a4d91776be94
marselek <titanlook20@gmail.com>
2018-01-15 17:26:03 +0100

    feature: preuzimanje liste proizvodaca i promjena izgleda url-a za slanje zahtjeva za pretragu

--- traziRobu.php
+++ traziRobu.php
@@ -2,41 +2,97 @@
 ob_start();
 header('Content-Type: text/html; charset=utf-8');
 ini_set("allow_url_fopen", 1);
-$stranica = "https://www.links.hr";
-$trazilica= "/hr/search?q=";
-$pojamZaPretragu = "grafička"; //Trenutno hardcoded 
-$url = $stranica. $trazilica . urlencode($pojamZaPretragu);
 
+$stranica = "https://www.links.hr";
+$trazilica = "/hr/search?q=";
+$pojamZaPretragu = "procesor"; //Trenutno hardcoded
+$dodatak = "&adv=true&adv=false"; //??? vjerojatno treba za search
+$proizvodac = 658; // AMD --- 0 ako nije navedeno
+$cMin = 1000;
+$cMax = 20000;
+
+$asc = "&orderby=10";
+$desc = "&orderby=11";
+
+$proizvodi = "&mid=";
+$cijenaOd = "&pf=";
+$cijenaDo = "&pt=";
+
+$url = $stranica . $trazilica;
 $lines = file($url);
 
-$nadjeno = TRUE;
+//filteri
+$filterProizvodac = "";
+$filterCijenaMin = "";
+$filterCijenaMax = "";
 
 
-for ($index = 0; $index < count($lines); $index++) {
-    $polje = array();
-    if (strpos($lines[$index], "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima")) {
-        echo "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima <br/>";
-        $nadjeno = FALSE;
+$nadjeno = FALSE;
+
+for ($i = 0; $i < count($lines); $i++) {
+    if (strpos($lines[$i], "<label for=\"mid\">Proizvođač:</label>")) {
+        $i = $i + 2;
+        while (strpos($lines[$i], "</select>") === FALSE) {
+            $pro[] = parsirajProizvodace($lines[$i]);
+            $i++;
+        }
         break;
     }
-    if (strpos($lines[$index], "div class=\"product-item\"")) {
+}
+//echo json_encode($pro, JSON_UNESCAPED_UNICODE);
+
+
+//url za pretragu
+if ($filterProizvodac !== "") {
+    $rez = nadjiProizvodaca($filterProizvodac, $pro);
+    if($rez === FALSE){
+        
+    }
+    $proizvodac = $filterProizvodac;
+}
+if ($filterCijenaMin !== "") {
+    $cMin = $filterCijenaMin;
+}
+if ($filterCijenaMax !== "") {
+    $cMax = $filterCijenaMax;
+}
+
+$url1 = $stranica . $trazilica . urlencode($pojamZaPretragu)
+        . $proizvodi . $proizvodac
+        . $dodatak
+        . $cijenaOd . $cMin
+        . $cijenaDo . $cMax
+        . $asc;
+echo $url1 . "<br/>";
+
+$lines1 = file($url1);
+
+for ($index = 0; $index < count($lines1); $index++) {
+    $polje = array();
+    if (strpos($lines1[$index], "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima")) {
+        echo "Nisu nađeni proizvodi koji odgovaraju zadanim kriterijima <br/>";
+        break;
+    }
+    if (strpos($lines1[$index], "div class=\"product-item\"")) {
         for ($i = $index; $i < $index + 54; $i++) {
-            $polje[] = $lines[$i];
+            $polje[] = $lines1[$i];
         }
         $obj[] = parsiranjeProizvoda($polje);
-        //break;
+        if (!$nadjeno) {
+            $nadjeno = TRUE;
+        }
         unset($polje);
     }
 }
 
 if ($nadjeno) {
-    echo json_encode($obj, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    echo json_encode($obj, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 }
 
 function parsiranjeProizvoda($polje) {
     $naziv = "";
     $cijena = "";
-    $link= "";
+    $link = "";
     for ($i = 0; $i < count($polje); $i++) {
         if (strpos($polje[$i], "product-title")) {
             $naziv = parsirajIme($polje[$i + 1]);
@@ -48,8 +104,8 @@ function parsiranjeProizvoda($polje) {
             continue;
             //echo "Cijena: " . $cijena . "<br>";
         }
-        if(strpos($polje[$i], "a href")){
-            $link= parsirajLink($polje[$i]);
+        if (strpos($polje[$i], "a href")) {
+            $link = parsirajLink($polje[$i]);
         }
     }
     $obj = (object) ['naziv' => $naziv, 'link' => $link, 'cijena' => $cijena];
@@ -67,10 +123,29 @@ function parsirajCijenu($linija) {
     return $cijena;
 }
 
-function parsirajLink($linija){
+function parsirajLink($linija) {
     preg_match("/\"(.*?)\"/", $linija, $match);
     return $match[1];
 }
+
+function parsirajProizvodace($linija) {
+    preg_match("/\"(.*)\">(.*?)</", $linija, $match);
+    $id = $match[1];
+    $proizvodac = $match[2];
+    $obj = (object) ['proizvodac' => $proizvodac, 'id' => $id];
+    return $obj;
+}
+
+function nadjiProizvodaca($naziv, $pro) {
+    foreach ($pro as $object) {
+        if ($object->proizvodac === $naziv) {
+            return $object->id;
+        }
+    }
+    return FALSE;
+}
+
+nadjiProizvodaca("AMD", $pro);
 ?>
 <!DOCTYPE html>
 <!--
