48277212dddd903176e33b470418812ccd232ac6
zeko868 <zeko868@hotmail.com>
2018-01-16 18:08:15 +0100

    popravljen pogrešno zakomentiran kod s višelinijskim komentarima
    popravljen pogrešno zakomentiran kod s višelinijskim komentarima
    (višelinijski komentar se nalazi unutar višelinijskog komentara te zbog
    toga ostaje jedna oznaka '*\' viška što uzrokuje pogrešku)

--- interpretirajZahtjev.php
+++ interpretirajZahtjev.php
@@ -265,14 +265,14 @@ $response = [
 	'message' => [ 'attachment' => $answer ]
 ];
 
-/*$ch = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=$accessToken");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-	$result = curl_exec($ch);
-}
-curl_close($ch);*/
+//$ch = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=$accessToken");
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+//if(!empty($input)){
+//	$result = curl_exec($ch);
+//}
+//curl_close($ch);
 echo json_encode($response);
 
 exit();*/
