4c5739d7f37aea0eaa350b12bca6d68b243bb036
zeko868 <zeko868@hotmail.com>
2018-01-17 06:58:43 +0100

    vraćeno lansiranje podataka prema Facebooku
    vraćeno lansiranje podataka prema Facebooku

--- bot.php
+++ bot.php
@@ -226,4 +226,13 @@ if(strpos($command,'/hr/') === 0){
 	}
 	
 }
-exit();
+
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . ACCESS_TOKEN);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+if($command != ""){
+	$result = curl_exec($ch);
+	var_dump($result);
+}
+curl_close($ch);
