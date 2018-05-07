75bc4b7367155b7529db7e6e4969e080d7b35f59
marmihajl <marmihajl@foi.hr>
2018-05-07 23:55:29 +0200

    string vracen u bot.php

--- interpretirajZahtjev.php
+++ interpretirajZahtjev.php
@@ -113,8 +113,7 @@ function NLPtext($translatedText){
         $string .= $nlp['proizvodac'] . ' ';
     if(isset($nlp['proizvod'])){
 		$string .= $nlp['proizvod'];
-		$conn = pg_connect('postgres://gsnnkdcbycpcyq:ba69093c4619187587610e80e188d4f812627530798ef14d3133bd3541b00290@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dedt0mj008catq');
-		$result = pg_query("INSERT INTO pregledavanja(id_facebook,string_pretrage,datum_pretrage) VALUES ('$senderId','" . $nlp['proizvod'] . "','$datumString');");	
+			
 	}
         
     
