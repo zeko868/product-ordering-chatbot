67ab96f046fbb57d78c63463baaa3b1e2e874294
marmihajl <marmihajl@foi.hr>
2018-05-08 00:00:15 +0200

    string pretrage iz bot.php u traziRobu.php

--- bot.php
+++ bot.php
@@ -240,8 +240,6 @@ $nlpText['tekst'] = urediIzlaz($translatedOutputText);
 $datum = new DateTime();
 $datumString = $datum->format("Y-m-d H:i:s");
 
-$conn = pg_connect('postgres://gsnnkdcbycpcyq:ba69093c4619187587610e80e188d4f812627530798ef14d3133bd3541b00290@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dedt0mj008catq');
-$result = pg_query("INSERT INTO pregledavanja(id_facebook,string_pretrage,datum_pretrage) VALUES ('$senderId','" . $nlpTekst['tekst'] . "','$datumString');");
 
 require './traziRobu.php';
 
