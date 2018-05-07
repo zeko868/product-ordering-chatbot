6911080834139d222570f9d28a70b89ea6c9375a
marmihajl <marmihajl@foi.hr>
2018-05-08 01:36:37 +0200

    dodan query i ispis rezultata queryja

--- akcija.php
+++ akcija.php
@@ -21,7 +21,12 @@ for ($index = 0; $index < count($lines1); $index++) {
     }
 }
 
-var_dump($obj);
+$conn = pg_connect('postgres://gsnnkdcbycpcyq:ba69093c4619187587610e80e188d4f812627530798ef14d3133bd3541b00290@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dedt0mj008catq');
+$result = pg_query("select distinct id_facebook, string_pretrage from pregledavanja where datum_pretrage <= '2018-05-07 22:02:00' and string_pretrage != '';");
+
+while($r = $result->fetch_assoc()){
+	var_dump($r);
+}
 
 function parsiranjeProizvoda($polje) {
     for ($i = 0; $i < count($polje); $i++) {
