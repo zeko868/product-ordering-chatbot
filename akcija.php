a940f1a44bccdaa37337dd9103f7f9802c123a81
marmihajl <marmihajl@foi.hr>
2018-05-08 17:00:27 +0200

    skripta akcija salje reklame korisniku, ali se ne prikazuju slike

--- akcija.php
+++ akcija.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+const ACCESS_TOKEN = 'EAACN8hwDY8QBAEcLkz9b9FZB2QXVgr92ZBduX8cEU1rfZBR7kOtzurRUtiWkZCan496HmhLyiWLnk86RAKsfMSiYKxZBdnIC6KftcZBy7EODHgPBERWpjFZCgqvPYWGUQyutGc76VccANwiCvrPxa9BCO7f3jnbTs2jXjZCzXk06OgZDZD';
 
 
 $url1 = "https://www.links.hr/hr/discounted-products?specFilters=1904";
@@ -34,21 +34,66 @@ $result = pg_query("select distinct id_facebook, string_pretrage from pregledava
 
 while($discountInfo = pg_fetch_array($result, null, PGSQL_ASSOC)){
 	$pretrage = explode(" ", $discountInfo["string_pretrage"]);
-	
-	
+	//var_dump($pretrage);
+	$buttons = array();
 	foreach($pretrage as $p){
 		
-		
 		foreach($obj as $o){
-			if(strpos($p, $o->naziv ) !== false){
-				var_dump($pretrage);
+			if(strpos($o->naziv,$p ) !== false){
+				//echo 'https://www.links.hr' . $o->link . '#quickTabs';
+				array_push($buttons, array(
+					'title' => htmlspecialchars_decode($o->naziv, ENT_QUOTES),
+					'image_url' => $obj[$i]->slika,
+					'subtitle' => htmlspecialchars_decode($o->naziv . ', cijena: ' . $obj[$i]->cijena, ENT_QUOTES),
+					'default_action' => [
+						'type' => 'web_url',
+						'url' => 'https://www.links.hr/hr/tipkovnica-logitech-k375s-bluetooth-stalak-za-smartphone-tablet-unifying-receiver-bezicna-crna-101200320#quickTabs',
+						'messenger_extensions' => true,
+						'webview_height_ratio'=> 'TALL'
+					],
+					'buttons' => array(
+						array(
+							'type' => 'postback',
+							'payload' => $o->link,
+							'title' => 'Naruči proizvod'
+							)
+						)
+					)
+				);
 			}
 		}
 		
+		
 	}
+	
+	$answer = [
+		'type'=>'template',
+		'payload'=>[
+			'template_type'=>'generic',
+			'elements'=> $buttons
+		]
+	];
+
+	replyBackSpecificObject([ 'attachment' => $answer ], $discountInfo["id_facebook"]);
 }
+
 pg_free_result($result);
 
+function replyBackSpecificObject($answer, $senderId) {
+	pg_close();
+	$response = [
+		'messaging_type' => 'RESPONSE',
+		'recipient' => [ 'id' => $senderId ],
+		'message' => $answer
+	];
+	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . ACCESS_TOKEN);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	exit();
+}
 
 
 function parsiranjeProizvoda($polje) {
