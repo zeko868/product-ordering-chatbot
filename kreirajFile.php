54b9af4f9b5611b4e6f5069c2adf15a0e2b9a079
marmihajl <marmihajl@foi.hr>
2018-05-10 20:54:57 +0200

    citanje filea i explodanje sadrzaja po \n

--- kreirajFile.php
+++ kreirajFile.php
@@ -1,5 +1,9 @@
 <?php
 $myfile = fopen("1155662414560805.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("1155662414560805.txt"));
+$fileContent = fread($myfile,filesize("1155662414560805.txt"));
 fclose($myfile);
+
+$fileContentArray = explode("\n", $fileContent);
+
+var_dump($fileContentArray);
 ?>
