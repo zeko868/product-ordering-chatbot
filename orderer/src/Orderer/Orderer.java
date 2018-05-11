77a3d9d0be47aa661c4701ad4de67d924e5c74d4
zeko868 <zeko868@hotmail.com>
2018-05-11 21:53:39 +0200

    Java program preskače naručivanje ako se koristi radi dohvata cijena
    Java program preskače naručivanje ako se koristi radi dohvata cijena kod pregleda stavki košarice

--- orderer/src/Orderer/Orderer.java
+++ orderer/src/Orderer/Orderer.java
@@ -68,124 +68,130 @@ public class Orderer {
                 productsInfo.add(price);
             }
 
-            try {
-                Thread.sleep(500);  // delay required before performing check-out
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            if (driver.getClass() != ChromeDriver.class) {
-                ((JavascriptExecutor)driver).executeScript("setLocation('/hr/login/checkoutasguest?returnUrl=%2Fhr%2Fcart');");
+            if (!pickupStore.isEmpty()) {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(500);  // delay required before performing check-out
                 } catch (InterruptedException ex) {
                     Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
-                driver.findElement(By.className("checkout-as-guest-button")).click();
+                if (driver.getClass() != ChromeDriver.class) {
+                    ((JavascriptExecutor)driver).executeScript("setLocation('/hr/login/checkoutasguest?returnUrl=%2Fhr%2Fcart');");
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
 
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                    driver.findElement(By.className("checkout-as-guest-button")).click();
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    JavascriptExecutor jsExecutor = ((JavascriptExecutor) driver);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_FirstName').value = arguments[0];", firstName);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_LastName').value = arguments[0];", lastName);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_Email').value = arguments[0];", email);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_Address1').value = arguments[0];", address);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_ZipPostalCode').value = arguments[0];", zipCode);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_City').value = arguments[0];", cityName);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_CountryId').value = arguments[0];", countryCode);
+                    jsExecutor.executeScript("document.getElementById('BillingNewAddress_PhoneNumber').value = arguments[0];", phoneNum);
+                }
+                else {
+                    driver.get("https://www.links.hr/hr/onepagecheckout");
+
+                    driver.findElement(By.id("BillingNewAddress_FirstName")).sendKeys(firstName);
+                    driver.findElement(By.id("BillingNewAddress_LastName")).sendKeys(lastName);
+                    driver.findElement(By.id("BillingNewAddress_Email")).sendKeys(email);
+                    driver.findElement(By.id("BillingNewAddress_Address1")).sendKeys(address);
+                    driver.findElement(By.cssSelector("label[for=\"BillingNewAddress_ZipPostalCode\"] ~ input")).sendKeys(zipCode);
+                    driver.findElement(By.id("BillingNewAddress_PhoneNumber")).sendKeys(phoneNum);
                 }
 
-                JavascriptExecutor jsExecutor = ((JavascriptExecutor) driver);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_FirstName').value = arguments[0];", firstName);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_LastName').value = arguments[0];", lastName);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_Email').value = arguments[0];", email);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_Address1').value = arguments[0];", address);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_ZipPostalCode').value = arguments[0];", zipCode);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_City').value = arguments[0];", cityName);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_CountryId').value = arguments[0];", countryCode);
-                jsExecutor.executeScript("document.getElementById('BillingNewAddress_PhoneNumber').value = arguments[0];", phoneNum);
-            }
-            else {
-                driver.get("https://www.links.hr/hr/onepagecheckout");
 
-                driver.findElement(By.id("BillingNewAddress_FirstName")).sendKeys(firstName);
-                driver.findElement(By.id("BillingNewAddress_LastName")).sendKeys(lastName);
-                driver.findElement(By.id("BillingNewAddress_Email")).sendKeys(email);
-                driver.findElement(By.id("BillingNewAddress_Address1")).sendKeys(address);
-                driver.findElement(By.cssSelector("label[for=\"BillingNewAddress_ZipPostalCode\"] ~ input")).sendKeys(zipCode);
-                driver.findElement(By.id("BillingNewAddress_PhoneNumber")).sendKeys(phoneNum);
-            }
+                driver.findElement(By.cssSelector("#billing-buttons-container > .new-address-next-step-button")).click();
 
-
-            driver.findElement(By.cssSelector("#billing-buttons-container > .new-address-next-step-button")).click();
-
-            while (true) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                if ("none".equals(driver.findElement(By.id("billing-please-wait")).getCssValue("display"))) {
-                    break;
-                }
-            }
-            
-            if (driver.getClass() == ChromeDriver.class) {
-                ((JavascriptExecutor) driver).executeScript("document.getElementById('shipping-buttons-container').scrollIntoView();");
-            }
-            driver.findElement(By.cssSelector("#shipping-buttons-container .new-address-next-step-button")).click();
-            
-            while (true) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                if ("none".equals(driver.findElement(By.id("shipping-please-wait")).getCssValue("display"))) {
-                    break;
-                }
-            }
-            
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            Select select = new Select(driver.findElement(By.id("shippingoption")));
-            if (pickupStore.equals("dostava")) {
-                select.selectByIndex(0);
-            }
-            else {
-                List<WebElement> options = select.getOptions();
-                int optionsNum = options.size();
-                for (int i=0; i<optionsNum; i++) {
-                    WebElement option = options.get(i);
-                    if (option.getAttribute("value").contains(pickupStore)) {
-                        select.selectByIndex(i);
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    if ("none".equals(driver.findElement(By.id("billing-please-wait")).getCssValue("display"))) {
                         break;
                     }
                 }
+
+                if (driver.getClass() == ChromeDriver.class) {
+                    ((JavascriptExecutor) driver).executeScript("document.getElementById('shipping-buttons-container').scrollIntoView();");
+                }
+                driver.findElement(By.cssSelector("#shipping-buttons-container .new-address-next-step-button")).click();
+
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    if ("none".equals(driver.findElement(By.id("shipping-please-wait")).getCssValue("display"))) {
+                        break;
+                    }
+                }
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                Select select = new Select(driver.findElement(By.id("shippingoption")));
+                if (pickupStore.equals("dostava")) {
+                    select.selectByIndex(0);
+                }
+                else {
+                    List<WebElement> options = select.getOptions();
+                    int optionsNum = options.size();
+                    for (int i=0; i<optionsNum; i++) {
+                        WebElement option = options.get(i);
+                        if (option.getAttribute("value").contains(pickupStore)) {
+                            select.selectByIndex(i);
+                            break;
+                        }
+                    }
+                }
+
+                driver.findElement(By.className("shipping-method-next-step-button")).click();
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                driver.findElement(By.className("payment-method-next-step-button")).click();
+
+                try {
+                    Thread.sleep(2000);  // delay required before performing check-out
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                String totalCost = driver.findElement(By.cssSelector(".product-price.order-total > strong")).getText();
+
+                driver.findElement(By.id("termsofservice")).sendKeys(" "); // hitting space button (un)checks checkbox
+
+                //driver.findElementByClassName("confirm-order-next-step-button").click();  // final step
+
+                System.out.println(totalCost);
             }
-
-            driver.findElement(By.className("shipping-method-next-step-button")).click();
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            driver.findElement(By.className("payment-method-next-step-button")).click();
-
-            try {
-                Thread.sleep(2000);  // delay required before performing check-out
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Orderer.class.getName()).log(Level.SEVERE, null, ex);
+            else {
+                System.out.println(""); // empty line so that result in both cases is consistent (in same format)
             }
             
-            String totalCost = driver.findElement(By.cssSelector(".product-price.order-total > strong")).getText();
-            
-            driver.findElement(By.id("termsofservice")).sendKeys(" "); // hitting space button (un)checks checkbox
-
-            //driver.findElementByClassName("confirm-order-next-step-button").click();  // final step
-            
-            System.out.println(totalCost);
             for (String pi : productsInfo) {
                 System.out.println(pi);
             }
