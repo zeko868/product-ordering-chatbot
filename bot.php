47e7e10666c04512f1a4cb5517623bc0c159e407
marmihajl <marmihajl@foi.hr>
2018-05-13 15:00:11 +0200

    uklonjen ispis primljene poruke

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ $input = json_decode(file_get_contents('php://input'), true, 512, JSON_BIGINT_AS
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $response = null;
 $command = '';
-replyBackWithSimpleText(json_encode($input));
+
 if ($messageInfo = $input['entry'][0]['messaging'][0]) {
 	$conn = pg_connect('postgres://gsnnkdcbycpcyq:ba69093c4619187587610e80e188d4f812627530798ef14d3133bd3541b00290@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dedt0mj008catq');
 	$result = pg_query("INSERT INTO user_account VALUES ('$senderId');");	// this is performed whether the user id is already in database or not - all the other table attributes are nullable, so their values don't need to be explicitly set
